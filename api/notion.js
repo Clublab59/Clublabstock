d1527bad807bffc1e7bf73b3bab899574b9b6b80
Clublab59 <nous@clublab.fr>
2026-04-29 11:33:00 +0200

    Update notion.js

--- api/notion.js
+++ api/notion.js
@@ -1,3 +1,11 @@
+export const config = {
+  api: {
+    bodyParser: {
+      sizeLimit: '1mb',
+    },
+  },
+};
+
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
@@ -9,10 +17,19 @@ export default async function handler(req, res) {
 
   try {
     let bodyData = req.body;
+    
+    if (!bodyData) {
+      const buffers = [];
+      for await (const chunk of req) {
+        buffers.push(chunk);
+      }
+      bodyData = JSON.parse(Buffer.concat(buffers).toString());
+    }
+    
     if (typeof bodyData === 'string') {
       bodyData = JSON.parse(bodyData);
     }
-    
+
     const { method, path, key, body } = bodyData;
 
     const response = await fetch('https://api.notion.com/v1' + path, {
